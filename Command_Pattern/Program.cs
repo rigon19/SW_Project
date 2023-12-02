70374751cca684457cfa1bbe807cbac3785230c6
rigon19 <106772957+rigon19@users.noreply.github.com>
2023-12-02 20:45:26 +0100

    First Commit

---
+++ Command_Pattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using Command_Pattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Character character = new Character();
+        ICommand Move= new MoveForwards(character);
+        ICommand Attack = new AttackCommand(character);
+        ICommand Stop = new StoppedMoving(character);
+
+        RemoteControl remote = new RemoteControl();
+
+        
+        remote.SetCommand(Move);
+        remote.DoAction();
+        remote.SetCommand(Attack);
+        remote.DoAction();
+        remote.UndoAction();
+        remote.SetCommand(Stop);
+        remote.DoAction();
+    }
+}
